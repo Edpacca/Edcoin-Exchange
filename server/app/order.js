f3d2161081919efcf0ea858bd867b8f6b0895128
Eddie Pace <epace@scottlogic.com>
2021-08-10 15:09:40 +0100

    Revert "create basic order class"
    This reverts commit 0cb349826592dc3b7c1f50154c4c6680bf780162.

--- server/app/order.js
+++
@@ -1,9 +0,0 @@
-class Order {
-    constructor(account, price, quantity, action) {
-        this.account = account;
-        this.quantity = quantity;
-        this.price = price;
-        this.action = action;
-        this.orderTime = Date.now();
-    }
-}
