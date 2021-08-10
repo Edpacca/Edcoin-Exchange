892df127cac4faa48f07dfa4688789b02a2c84da
Eddie Pace <epace@scottlogic.com>
2021-08-10 15:11:25 +0100

    create basic order class

---
+++ server/app/order.js
@@ -0,0 +1,9 @@
+class Order {
+    constructor(account, price, quantity, action) {
+        this.account = account;
+        this.quantity = quantity;
+        this.price = price;
+        this.action = action;
+        this.orderTime = Date.now();
+    }
+}
