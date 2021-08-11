2464111e51a82363fe007da5dfa20e52aa293e74
Eddie Pace <epace@scottlogic.com>
2021-08-11 16:18:10 +0100

    change test to use new validateOrder function and fix error

--- server/test/matcher.test.js
+++ server/test/matcher.test.js
@@ -9,19 +9,19 @@ describe("Matcher", () => {
     });
 
     it("returns false if matchNewOrder is passed an invalid order object", () => {
-        expect(matcher.matchNewOrder("badOrder")).toBe(false);
+        expect(matcher.validateOrder("badOrder")).toBe(false);
     });
 
     it("returns false if the order object has invalid action", () => {
-        expect(matcher.matchNewOrder(new Order(1, 1, 1, "INVALID_ACTION"))).toBe(false);
+        expect(matcher.validateOrder(new Order(1, 1, 1, "INVALID_ACTION"))).toBe(false);
     });
 
     it("returns true if the order object has valid SELL action", () => {
-        expect(matcher.matchNewOrder(new Order(1, 1, 1, "SELL"))).toBe(false);
+        expect(matcher.validateOrder(new Order(1, 1, 1, "SELL"))).toBe(true);
     });
 
     it("returns true if the order object has valid BUY action", () => {
-        expect(matcher.matchNewOrder(new Order(1, 1, 1, "BUY"))).toBe(false);
+        expect(matcher.validateOrder(new Order(1, 1, 1, "BUY"))).toBe(true);
     });
     
 });
