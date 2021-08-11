e30b1871068a1521dbc22be9668fea643320c0be
Eddie Pace <epace@scottlogic.com>
2021-08-11 09:26:54 +0100

    create tests for matchNewOrder function

--- server/test/matcher.test.js
+++ server/test/matcher.test.js
@@ -1,4 +1,5 @@
 const Matcher = require("../app/matcher");
+const Order = require("../app/order");
 
 describe("Matcher", () => {
     let matcher;
@@ -7,7 +8,15 @@ describe("Matcher", () => {
         matcher = new Matcher();
     });
 
-    it("fails this test", () => {
-        expect(true).toBe(false);
+    it("returns false if matchNewOrder is passed an invalid order object", () => {
+        expect(matcher.matchNewOrder("badOrder")).toBe(false);
+    });
+
+    it("returns false if the order object has invalid action", () => {
+        expect(matcher.matchNewOrder(new Order(1, 1, 1, "INVALID_ACTION"))).toBe(false);
+    });
+
+    it("returns true if the order object has valid action", () => {
+        expect(matcher.matchNewOrder(new Order(1, 1, 1, "SELL"))).toBe(false);
     });
 });
