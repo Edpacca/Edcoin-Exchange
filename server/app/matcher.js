a580ff6c0c706fe1ead11607423af3bb9ee553d7
Eddie Pace <epace@scottlogic.com>
2021-08-16 15:16:32 +0100

    refactor matcher

--- server/app/matcher.js
+++ server/app/matcher.js
@@ -7,20 +7,24 @@ class Matcher {
 
     matchNewOrder(newOrder) {
 
-        if (!this.validateOrder(newOrder)) return false;
-        if (this.orders.length == 0) return false;
+        if (!this.validateOrder(newOrder)) {
+            return false;
+        }
+        if (this.orders.length == 0) {
+            return false;
+        }
 
-        let potentialMatches = this.getPotentialMatches(newOrder);
-        
-        return potentialMatches.length == 0 
-            ? false 
-            : potentialMatches;
+        return this.getPotentialMatches(newOrder);
     };
 
     validateOrder(newOrder) {
 
-        if (!(newOrder instanceof Order)) return false;
-        if (newOrder.action !== "BUY" && newOrder.action !== "SELL") return false;
+        if (!(newOrder instanceof Order)) {
+            return false;
+        }
+        if (newOrder.action !== "BUY" && newOrder.action !== "SELL") {
+            return false;
+        } 
 
         return true;
     }
@@ -39,7 +43,10 @@ class Matcher {
             potentialMatches = potentialMatches.filter(o => o.price >= newOrder.price);
             potentialMatches.sort((a, b) => (a.price - b.price));
 
-        } else return false;
+        } else {
+
+            return false;
+        }
 
         return potentialMatches;
     };
@@ -52,7 +59,6 @@ class Matcher {
 
         return this.orders.filter(o => o.action == opposingAction);
     }
-
 };
 
 module.exports = Matcher;
