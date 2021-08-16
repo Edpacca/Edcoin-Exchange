9188d38bfc7aae1901d7d5a9f8b835a22ecc5f4b
Eddie Pace <epace@scottlogic.com>
2021-08-16 16:50:04 +0100

    implement trading within POST function

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,35 +1,24 @@
+const _ = require('lodash'); 
 const Order = require('../app/order');
+const tradeManager = require('../app/tradeManager');
 const db = require('../db/dbManager');
 
 let ordersDb = db.getOrders();
-/* 
-3. Integrate the express framework into your server-side code.
-
-4. Adapt your server-side code to store aggregated order book / private order book /
-trade data. The aggregated order book should aggregate on price, the number of
-assets being sought or offered at each price level.
-
-5. Allow clients to retrieve the above data from the server.
-
-6. Allow clients to send new orders that are passed on to the matcher.
-
-7. When a new order is received, ensure the updated aggregated order book / private
-order book and trade data is returned in the response.
-
-8. Extend your tests to cover any newly introduced or changed behaviour.
-*/
+let tradesDb = [];
 
 const appRouter = function(app) {
 
-    // 5 GET aggregated orders (dict) => return list
-    // 5 GET trade data => 
-    // 6 POST create new order => send to matcher
-
+    // GET all orders unsorted
     app.get("/orders", (request, result) => {
-
         result.status(200).send(ordersDb);
     });
 
+    // GET all trades
+    app.get("/trades", (request, result) => {
+        result.status(200).send(tradesDb);
+    });
+
+    // GET order by uuid
     app.get("/order/:id", (request, result) => {
         
         let order = ordersDb.find(o => o.id === request.params.id);
@@ -42,9 +31,11 @@ const appRouter = function(app) {
 
     });
 
+    // create new order, match and perform trades
+    // return message log of activity
     app.post("/order", (request, result) => {
 
-        // only verifying order direction for mock model
+        // only verifying order direction for test
         if (request.body.action !== "BUY" && request.body.action !== "SELL") {
 
             result.status(400).send("invalid action: cannot create order");
@@ -56,10 +47,35 @@ const appRouter = function(app) {
                 request.body.quantity,
                 request.body.price,
                 request.body.action);
-    
-            ordersDb.push(order);
 
-            result.status(201).send(order);
+            // pass order to trade manager
+            // trade data contains message and trades[]
+            new tradeManager.handleNewTrade(order, ordersDb);
+            let tradeData = tradeManager.getTradeData();
+
+            let message = `New order: ${order.action} ${order.price} at ${request.body.quantity}\n\n`;
+            message += tradeData.message;
+
+            if (order.quantity !== 0) {
+                ordersDb.push(order);
+            } 
+
+            if (tradeData.trades.length !== 0) {
+
+                tradesDb.push(tradeData.trades);
+
+                message += 'Trades completed:\n';
+                
+                for (let t of tradeData.trades) {
+                    message += `
+                    ${t.existingOrder.action} ${t.tradeQuantity} at ${t.tradePrice} 
+                    ${t.existingOrder.id} -> ${t.newOrder.id}\n\n`
+                }
+            }
+
+            _.remove(ordersDb, function(order){return order.quantity == 0});
+            
+            result.status(201).send(message);
         }
 
     });
