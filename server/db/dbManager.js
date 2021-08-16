fab79999e92e57c2538813d886065d83b55cd9a3
Eddie Pace <epace@scottlogic.com>
2021-08-16 15:06:00 +0100

    implement basic db manager

---
+++ server/db/dbManager.js
@@ -0,0 +1,24 @@
+const mockOrders = require("./mockOrders");
+
+let ordersDb = [];
+let isDbInitialised = false;
+
+function initialiseDb() {
+
+    ordersDb = mockOrders.getOrders();
+    isDbInitialised = true;
+    return ordersDb;
+}
+
+function aggregateOrders() {
+
+}
+
+function getOrders() {
+
+    return isDbInitialised 
+        ? ordersDb 
+        : initialiseDb();
+}
+
+module.exports = { getOrders, aggregateOrders } ;
