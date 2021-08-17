8ef02b7ddd350cdef450874908153f23ea794fa9
Eddie Pace <epace@scottlogic.com>
2021-08-17 09:23:04 +0100

    group tests for same methods into describe block

--- server/test/matcher.test.js
+++ server/test/matcher.test.js
@@ -2,6 +2,7 @@ const Matcher = require("../app/matcher");
 const Order = require("../app/order");
 
 describe("Matcher", () => {
+
     let matcher;
     let orders = [
         new Order(0, 20, 1, "BUY"),
@@ -35,64 +36,72 @@ describe("Matcher", () => {
         });
     });
 
+
+    describe("filter orders", () => {
+
+        it("filterOrders returns an array of SELL orders if passed a BUY order", () => {
+        
+            let isOnlySellActions;
+    
+            matcher.filterOrders("BUY").forEach(order => {
+                isOnlySellActions = (order.action == "SELL")
+            });
+    
+            expect(isOnlySellActions).toBe(true);
+        });
+    
+        it("filterOrders returns an array of BUY orders if passed a SELL order", () => {
+            
+            let isOnlyBuyActions;
+    
+            matcher.filterOrders("SELL").forEach(order => {
+                isOnlyBuyActions = (order.action == "BUY")
+            });
+    
+            expect(isOnlyBuyActions).toBe(true);
+        });
+    })
+
+    describe("match new order", () => {
+
+        it("matchNewOrder returns empty array if no matches are found", () => {
+            // all sell orders are above 10
+            let order = new Order(1, 10, 1, "BUY");
+    
+        
+            expect(matcher.matchNewOrder(order).length).toBe(0);
+        });
+    
+        it("matchNewOrder returns an array if passed a valid Order with matches", () => {
+            
+            let order = new Order(1, 30, 1, "BUY");
+            expect(Array.isArray(matcher.matchNewOrder(order))).toBe(true);
+    
+        });
+    
+        it("matchNewOrder returns an array of valid Orders if passed a valid Order with matches", () => {
+            
+            let newOrder = new Order(1, 30, 1, "BUY");
+            let isOrder;
+    
+            matcher.matchNewOrder(newOrder).forEach(order => {
+                isOrder = matcher.validateOrder(order);
+            });
+    
+            expect(isOrder).toBe(true);
+        });
+    
+        if("matchNewOrder returns false if passed an empty database", () => {
+            
+            matcher = new Matcher([]);
+            expect(matcher.matchNewOrder(new Order(1, 1, 1, "SELL"))).toBe(false);    
+                
+        });
+    
+    })
+
     it("getPotentialMatches returns false if the order object has invalid action", () => {
         expect(matcher.getPotentialMatches(new Order(1, 1, 1, "INVALID_ACTION"))).toBe(false);
-    });
-
-    it("filterOrders returns an array of SELL orders if passed a BUY order", () => {
-        
-        let isOnlySellActions;
-
-        matcher.filterOrders("BUY").forEach(order => {
-            isOnlySellActions = (order.action == "SELL")
-        });
-
-        expect(isOnlySellActions).toBe(true);
-    });
-
-    it("filterOrders returns an array of BUY orders if passed a SELL order", () => {
-        
-        let isOnlyBuyActions;
-
-        matcher.filterOrders("SELL").forEach(order => {
-            isOnlyBuyActions = (order.action == "BUY")
-        });
-
-        expect(isOnlyBuyActions).toBe(true);
-    });
-
-    it("matchNewOrder returns empty array if no matches are found", () => {
-        // all sell orders are above 10
-        let order = new Order(1, 10, 1, "BUY");
-
-    
-        expect(matcher.matchNewOrder(order).length).toBe(0);
-    });
-
-    it("matchNewOrder returns an array if passed a valid Order with matches", () => {
-        
-        let order = new Order(1, 30, 1, "BUY");
-        expect(Array.isArray(matcher.matchNewOrder(order))).toBe(true);
-
-    });
-
-    it("matchNewOrder returns an array of valid Orders if passed a valid Order with matches", () => {
-        
-        let newOrder = new Order(1, 30, 1, "BUY");
-        let isOrder;
-
-        matcher.matchNewOrder(newOrder).forEach(order => {
-            isOrder = matcher.validateOrder(order);
-        });
-
-        expect(isOrder).toBe(true);
-    });
-
-    if("matchNewOrder returns false if passed an empty database", () => {
-        
-        matcher = new Matcher([]);
-        expect(matcher.matchNewOrder(new Order(1, 1, 1, "SELL"))).toBe(false);    
-            
     });
 
 });
