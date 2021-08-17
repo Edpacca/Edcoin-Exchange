6bab5a9bba6e9c574295ce6b801a392cb3828b78
Eddie Pace <epace@scottlogic.com>
2021-08-17 12:50:51 +0100

    remove business logic from routes, change tradeManager to ServiceManager

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,10 +1,11 @@
 const _ = require('lodash'); 
 const Order = require('../app/order');
-const tradeManager = require('../app/tradeManager');
+const ServiceManager = require('../service/serviceManager');
 const db = require('../db/dbManager');
 
-let ordersDb = db.getOrders();
-let tradesDb = [];
+// Temporary location for Db rerences
+const tradesDb = [];
+const ordersDb = db.getOrders();
 
 const appRouter = function(app) {
 
@@ -24,7 +25,7 @@ const appRouter = function(app) {
         let order = ordersDb.find(o => o.id === request.params.id);
 
         if (order == null) {
-            result.status(400).send("Invalid id");
+            result.status(404).send("Invalid id");
         } else {
             result.status(200).send(order);
         };
@@ -32,51 +33,24 @@ const appRouter = function(app) {
     });
 
     // create new order, match and perform trades
-    // return message log of activity
     app.post("/order", (request, result) => {
 
         // only verifying order direction for test
         if (request.body.action !== "BUY" && request.body.action !== "SELL") {
-
             result.status(400).send("invalid action: cannot create order");
-
         } else {
 
-            let order = new Order(
+            const order = new Order(
                 request.body.account,
                 request.body.price,
                 request.body.quantity,
                 request.body.action);
 
-            // pass order to trade manager
-            // trade data contains message and trades[]
-            new tradeManager.handleNewTrade(order, ordersDb);
-            let tradeData = tradeManager.getTradeData();
-
-            let message = `New order: ${order.action} ${order.price} at ${request.body.quantity}\n\n`;
-            message += tradeData.message;
-
-            if (order.quantity !== 0) {
-                ordersDb.push(order);
-            } 
-
-            if (tradeData.trades.length !== 0) {
-
-                tradesDb.push(tradeData.trades);
-
-                message += 'Trades completed:\n';
-                
-                for (let t of tradeData.trades) {
-                    message += `
-                    ${t.existingOrder.action} ${t.tradeQuantity} at ${t.tradePrice} 
-                    ${t.existingOrder.id} -> ${t.newOrder.id}\n\n`
-                }
-            }
-
-            _.remove(ordersDb, function(order){return order.quantity == 0});
-            
-            result.status(201).send(message);
-        }
+            // pass order to serviceManager which handles matching and trading
+            const serviceManager = new ServiceManager(ordersDb, tradesDb);
+            serviceManager.handleNewOrder(order);
+            result.status(201).send(order);
+        };
 
     });
 
