04f44c28f45e88dbee271b6b9f23d0c382cf19cd
Eddie Pace <epace@scottlogic.com>
2021-08-26 16:56:35 +0100

    split reducer to handle orders only

--- client/src/app/ordersReducer.ts
+++ client/src/app/ordersReducer.ts
@@ -1,42 +1,44 @@
-export const initialState = {
-    orders: [
+import { DirectionType } from "../models/directionType";
+import { Order } from "../models/order";
+
+export const initialState: Order[] = [
         {
-            orderTime: "2021-08-26T10:02:36.712Z",
+            orderTime: new Date(),
             id: "f9a40020-2e52-473b-92de-2d65d3c6d774",
             account: 0,
             quantity: 46,
             price: 39.548511672746095,
-            action: "SELL"
+            direction: DirectionType.Sell
         },  
         {   
-            orderTime: "2021-08-26T10:02:36.712Z",
+            orderTime: new Date(),
             id: "ba8a32b9-2e68-4db2-89d8-74994ab2887e",
             account: 1,
             quantity: 12,
             price: 44.94696969930247,
-            action: "SELL"
+            direction: DirectionType.Sell
         },  
         {   
-            orderTime: "2021-08-26T10:02:36.712Z",
+            orderTime: new Date(),
             id: "5ab0ecdc-9ca5-471e-9c06-09560dc2ab7a",
             account: 2,
             quantity: 30,
             price: 77.09042122916647,
-            action: "BUY"
+            direction: DirectionType.Buy
         },  
         {   
-            orderTime: "2021-08-26T10:02:36.712Z",
+            orderTime: new Date(),
             id: "8423076d-10d3-46ff-a2b7-d6ebe715b532",
             account: 3,
             quantity: 32,
             price: 39.84791285233185,
-            action: "BUY"
+            direction: DirectionType.Buy
         },
-    ],
-    trades: []
-}
+    ]
 
-export default function rootReducer(state = initialState, action: {type: string, payload: any}) {
+export type RootState = ReturnType<typeof ordersReducer>
+
+export default function ordersReducer(state = initialState, action: any) {
 
     switch (action.type) {
         case 'orders/orderCreated': {
@@ -46,9 +48,6 @@ export default function rootReducer(state = initialState, action: {type: string,
             return state;
         }
         case 'orders/orderUpdated': {
-            return state;
-        }
-        case 'trades/tradeCreated': {
             return state;
         }
         default:
