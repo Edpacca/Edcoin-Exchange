ddc35bed7cf98cbcc37d86b2c2953957a74e5478
Eddie Pace <epace@scottlogic.com>
2021-08-26 13:26:41 +0100

    use Order interface in mockOrder

--- client/src/features/orderer/Orderer.tsx
+++ client/src/features/orderer/Orderer.tsx
@@ -1,13 +1,15 @@
 import { BuySellToggle } from './BuySellToggle';
 import { Button } from '@material-ui/core';
 import { v4 as uuid } from 'uuid';
+import { Order } from '../../models/order'
+import { DirectionType } from '../../models/directionType';
 
-const mockOrder = {
+const mockOrder: Order = {
     id: uuid(),
     orderTime: new Date(),
     account: 99,
     price: 20,
-    direction: "BUY",
+    direction: DirectionType.Buy,
     quantity: 5,
 }
 
