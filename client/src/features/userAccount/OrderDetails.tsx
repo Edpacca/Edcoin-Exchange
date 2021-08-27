196a310d60e93f467bf2bb18d68eed6430b1f5d2
Eddie Pace <epace@scottlogic.com>
2021-08-27 17:23:22 +0100

    implement orderDetails with data from store

--- client/src/features/userAccount/OrderDetails.tsx
+++ client/src/features/userAccount/OrderDetails.tsx
@@ -1,21 +1,27 @@
-export function RenderOrderDetails(props: any) {
+import { DirectionType } from "../../models/directionType";
+import { Order } from "../../models/order";
+import dateFormat from "dateformat"
+
+export function RenderOrderDetails(order: Order) {
     
-    const time = renderRow({name: "Time", data: props.orderTime});    
-    const direction = renderRow({name: "Direction", data: props.direction});    
-    const price = renderRow({name: "Price", data: props.price});    
-    const quantity = renderRow({name: "Quantity", data: props.quantity});    
+    const time = renderRow({name: "Time", data: dateFormat(order.orderTime)});    
+    const direction = renderRow({name: "Direction", data: order.direction});    
+    const price = renderRow({name: "Price", data: order.price});    
+    const quantity = renderRow({name: "Quantity", data: order.quantity});    
 
     return (
         <table className="table-order">
-            {time}
-            {direction}
-            {price}
-            {quantity}
+            <tbody>
+                {time}
+                {direction}
+                {price}
+                {quantity}
+            </tbody>
         </table>
     )
 }
 
-function renderRow(props: any) {
+function renderRow(props: {name: string, data: string | DirectionType | number}) {
     return (
         <tr>
             <td className="table-label">{props.name}</td>
