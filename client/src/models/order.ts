6ca1ea063cb2aaebc1da18042de9b78607bb190e
Eddie Pace <epace@scottlogic.com>
2021-08-27 11:51:58 +0100

    make id nullable

--- client/src/models/order.ts
+++ client/src/models/order.ts
@@ -2,7 +2,7 @@ import { DirectionType } from "./directionType";
 
 export interface Order {
     readonly orderTime: Date,
-    readonly id: string,
+    readonly id?: string,
     readonly account: number,
     readonly quantity: number,
     readonly price: number,
