f1fd2f1c65f8264377ae836f93f90022eb0b0149
Eddie Pace <epace@scottlogic.com>
2021-08-27 17:21:35 +0100

    change material ui components to standard html

--- client/src/features/orderer/BuySellToggle.tsx
+++ client/src/features/orderer/BuySellToggle.tsx
@@ -1,14 +1,11 @@
 import React, { MouseEventHandler } from 'react';
-import { ButtonGroup } from '@material-ui/core';
 
 export function BuySellToggle(props: {isBuying: boolean, onClick: MouseEventHandler}) {
     
     return(
-        <div>
-            <ButtonGroup>
-                <button className={getStyle(props.isBuying)} onClick={props.onClick}>BUY</button>
-                <button className={getStyle(!props.isBuying)} onClick={props.onClick}>SELL</button>
-            </ButtonGroup>
+        <div className="toggle-buttons">
+            <button className={getStyle(props.isBuying)} onClick={props.onClick}>BUY</button>
+            <button className={getStyle(!props.isBuying)} onClick={props.onClick}>SELL</button>
         </div>
     )
 }
