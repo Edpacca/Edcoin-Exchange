bd6b724e9773046b43d414c7431a9562ed15049a
Eddie Pace <epace@scottlogic.com>
2021-08-31 13:28:55 +0100

    update Navbar with UserNavbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import './App.css';
 import logo from './logo.svg';
-import { Navbar } from './features/navigation/Navbar';
+import { UserNavbar } from './features/navigation/UserNavbar';
 
 function App() {
   return (
     <div className="App">
         <img src={logo} className="App-logo" alt="logo" />
-        <div className="App-function">
-          <Navbar />
+        <div className="split-sections">
+          <div className="private-area" >
+            <UserNavbar />
+          </div>
         </div>
     </div>
   );
